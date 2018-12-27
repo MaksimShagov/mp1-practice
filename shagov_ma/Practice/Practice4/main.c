9669a2f0f1238e9896ffd250c27f02cb36b986f0
MaksimShagov <43129418+MaksimShagov@users.noreply.github.com>
2018-12-27 09:58:34 +0300

    Add files via upload

--- shagov_ma/Practice/Practice4/main.c
+++ shagov_ma/Practice/Practice4/main.c
@@ -4,107 +4,107 @@
 #define MAX 7
 #define MAX_LEN 17
 
-int codes[MAX] = { 1231, 3132, 1233, 3224, 5313, 6422, 1317};
-char names[MAX][MAX_LEN] = { "РҐР»РµР±","РњСЏСЃРѕ","Р’РѕРґР°","РЎС‹СЂ","РњРѕР»РѕРєРѕ","РљР°СЂС‚РѕС€РєР°","Р“РѕСЂРѕС…" };
+char codes[MAX][2] = { "1", "2", "3", "4", "5", "6", "7" };
+char names[MAX][MAX_LEN] = { "Хлеб","Мясо","Вода","Сыр","Молоко","Картошка","Горох" };
 int prices[MAX] = { 10, 20, 30, 40, 50, 60, 70 };
 int discounts[MAX] = { 2, 3, 23, 11, 5, 9, 9 };
 int user_choose1;
-int check[100] = {0};
+int check[100] = { 0 };
 
 void call_menu()
 {
-	printf("\nРњРµРЅСЋ С„СѓРЅРєС†РёР№\n");
-	printf(" 1 - СЃРєР°РЅРёСЂРѕРІР°С‚СЊ С‚РѕРІР°СЂ\n 2 - РІС‹РІРѕРґ РѕРїРёСЃР°РЅРёСЏ РѕС‚СЃРєР°РЅРёСЂРѕРІР°РЅРЅРѕРіРѕ С‚РѕРІР°СЂР°\n");
-	printf(" 3 - РґРѕР±Р°РІРёС‚СЊ РґР°РЅРЅС‹Рµ Рѕ С‚РѕРІР°СЂРµ РІ С‡РµРє\n 4 - СЃС„РѕСЂРјРёСЂРѕРІР°С‚СЊ С‡РµРє\n 5 - СЂР°СЃС‡РёС‚Р°С‚СЊ РёС‚РѕРіРѕРІСѓСЋ СЃСѓРјРјСѓ\n");
-	printf(" 6 - РІС‹С…РѕРґ РёР· РїСЂРѕРіСЂР°РјРјС‹\n")
+	printf("\nМеню функций\n");
+	printf(" 1 - сканировать товар\n 2 - вывод описания отсканированного товара\n");
+	printf(" 3 - добавить данные о товаре в чек\n 4 - сформировать чек\n 5 - расчитать итоговую сумму\n");
+	printf(" 6 - выход из программы\n");
 }
 
 void scan()
 {
-    int q;
-    do{
-        printf("Р’РІРµРґРёС‚Рµ С€С‚СЂРёС…-РєРѕРґ\n");
+	int q;
+	do {
+		printf("Введите штрих-код\n");
 		scanf("%d", &user_choose1);
-	    q = scan_error(user_choose1);
-	    } while(q == 0);
+		q = scan_error(user_choose1);
+	} while (q == 0);
 }
 
 int scan_error(int user_choose1)
 {
-    int i, q;
-    for (i = 0; i < MAX; i++)
-    {
-        q = 0;
-        if ((user_choose1 < 10000) && (user_choose1 >= 1) && (user_choose1 == codes[i]))
-    	{
-    		q++;
-    		break;
-    	}
-    }
-    if (q == 0)
-    {
-        printf("\nРўРѕРІР°СЂР° СЃ С‚Р°РєРёРј С€С‚СЂРёС…-РєРѕРґРѕРј РЅРµС‚");
-    	printf("\nР’РІРµРґРёС‚Рµ РЅРѕРІС‹Р№ С€С‚СЂРёС… РєРѕРґ");
-    }
+	int i, q;
+	for (i = 0; i < MAX; i++)
+	{
+		q = 0;
+		if ((user_choose1 < 10000) && (user_choose1 >= 1) && (user_choose1 == atoi(codes[i])))
+		{
+			q++;
+			break;
+		}
+	}
+	if (q == 0)
+	{
+		printf("\nТовара с таким штрих-кодом нет");
+		printf("\nВведите новый штрих код");
+	}
 	return q;
 }
 
 int search(int user_choose1)
 {
-    int i;
-    for (i = 0; i < MAX; i++)
-    {
-        if(user_choose1 == codes[i])
-        return i;
-    }
+	int i;
+	for (i = 0; i < MAX; i++)
+	{
+		if (user_choose1 == atoi(codes[i]))
+			return i;
+	}
 }
 
 void info_items()
 {
-    int q;
-    if (user_choose1 == -1)
+	int q;
+	if (user_choose1 == -1)
 	{
-		printf("\nР’С‹ РЅРµ РѕС‚СЃРєР°РЅРёСЂРѕРІР°Р»Рё С‚РѕРІР°СЂ\n");
+		printf("\nВы не отсканировали товар\n");
 		return;
 	}
 	q = search(user_choose1);
-	printf ("\nРўРѕРІР°СЂ  %s\nСЃРєРёРґРєР° РЅР° РЅРµРіРѕ %d\n", names[q], discounts[q]);
-	printf ("РёРјРµРµС‚ С†РµРЅСѓ Р±РµР· СЃРєРёРґРєРё %d\nСЃРѕ СЃРєРёРґРєРѕР№ %.2f", prices[q], prices[q]*(1-discounts[q]*0.01f));
+	printf("\nТовар  %s\nскидка на него %d\n", names[q], discounts[q]);
+	printf("имеет цену без скидки %d\nсо скидкой %.2f", prices[q], prices[q] * (1 - discounts[q] * 0.01f));
 }
 
 void print_items()
 {
 	int i, q = 0;
 	for (i = 0; i < MAX; i++)
+	{
+		if (check[i] != 0)
 		{
-			if (check[i]!=0)
-			{
-				q++;
-				printf("%d. %s -- %d -- %d -- %.0f\n", q, names[i], codes[i], check[i], prices[i]*(1-discounts[q]*0.01f));
-			}
+			q++;
+			printf("%d. %s -- %s -- %d -- %.0f\n", q, names[i], codes[i], check[i], prices[i] * (1 - discounts[q] * 0.01f));
 		}
+	}
 }
 
 void add()
 {
-    check[search(user_choose1)]++;
-	printf("\nРўРѕРІР°СЂ СѓСЃРїРµС€РЅРѕ РґРѕР±Р°РІР»РµРЅ РІ С‡РµРє\n");
+	check[search(user_choose1)]++;
+	printf("\nТовар успешно добавлен в чек\n");
 }
 
-total_price()
+void total_price()
 {
 	int i = 0;
 	float s = 0;
 	for (i = 0; i < MAX; i++)
 	{
-		s += check[i] * prices[i]*(1-discounts[i]*0.01f);
+		s += check[i] * prices[i] * (1 - discounts[i] * 0.01f);
 	}
-	printf("РћР±С‰Р°СЏ С†РµРЅР° %.0f\n", s);
+	printf("Общая цена %.0f\n", s);
 }
 void main()
 {
-	int i, pick;
-	srand((unsigned int) time(0));
+	int i, pick, q;
+	srand((unsigned int)time(0));
 	setlocale(LC_ALL, "Russian");
 	do {
 		call_menu();
@@ -117,5 +117,5 @@ void main()
 		case 4: print_items(); break;
 		case 5: total_price(); break;
 		}
-	} while(pick != 6);
+	} while (pick != 6);
 }
