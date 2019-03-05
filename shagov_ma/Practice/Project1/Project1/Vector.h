c364532d89e78cf387e6a58f501d72a5ca8b4acd
MaksimShagov <43129418+MaksimShagov@users.noreply.github.com>
2019-03-05 06:32:35 +0300

    Add files via upload

--- shagov_ma/Practice/Project1/Project1/Vector.h
+++ shagov_ma/Practice/Project1/Project1/Vector.h
@@ -6,10 +6,27 @@ class Vector
 	int* elements;
 public:
 	Vector();
-	Vector(Vector&);
+	Vector(const Vector&);
 	Vector(int);
 	Vector(int, int*);
 	~Vector();
+	
 	void PrintVector();
-	void getsize();
+	int getsize();
+	Vector& sum(Vector&);
+	Vector& dif(Vector&);
+	int length();
+	int scal(Vector&);
+
+	Vector& operator+(const Vector&);
+	Vector& operator=(const Vector&);
+	Vector& operator-(const Vector&);
+	Vector& operator*(int);
+	Vector& operator/(int);
+
+	int operator*(const Vector&);
+	bool operator==(const Vector&);
+
+	int& operator[](int index);
+	const int& operator[](int index) const;
 };
--- shagov_ma/Practice/Project1/Project1/main.cpp
+++ shagov_ma/Practice/Project1/Project1/main.cpp
@@ -3,7 +3,13 @@
 
 int main()
 {
-	Vector* Artem = new Vector(); //Будем знакомы, Артем
+	int *a;
+	a = new int[10];
+	Vector* Artem = new Vector();
+	Vector* Artem1 = new Vector(*Artem);
+	Vector* Artem2 = new Vector(10);
+	Vector* Artem3 = new Vector(10, a);//Будем знакомы, Артем
+	Artem->sum(*Artem1);
 	Artem->PrintVector();
 	int n;
 	std::cin >> n;
