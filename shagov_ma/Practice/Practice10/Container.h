57bf809923425134d97f6c0134d4328ed0324c5f
MaksimShagov <43129418+MaksimShagov@users.noreply.github.com>
2019-05-07 10:35:35 +0300

    Final

--- shagov_ma/Practice/Practice10/Container.h
+++ shagov_ma/Practice/Practice10/Container.h
@@ -21,6 +21,24 @@ public:
 	void print_all();
 };
 
+template <typename T, int maxsize>
+class Container<T*, maxsize>
+{
+	T** arr;
+	int size;
+public:
+	Container();
+	Container(const Container<T*, maxsize>&);
+	~Container();
+	bool IsFull() const;
+	bool IsEmpty() const;
+	int find(T) const;
+	void Add(T);
+	void Delete(T);
+	T& operator[](int);
+	void print_all();
+};
+
 template<typename T, int maxsize>
 Container<T, maxsize>::Container()
 {
@@ -65,7 +83,7 @@ Container<T, maxsize>::~Container()
 }
 
 template<typename T, int maxsize>
-Container<T, maxsize>::~Container()
+Container<T*, maxsize>::~Container()
 {
 	size = 0;
 	delete[] arr;
@@ -78,7 +96,19 @@ bool Container<T, maxsize>::IsFull() const
 }
 
 template<typename T, int maxsize>
+bool Container<T*, maxsize>::IsFull() const
+{
+	return (size == maxsize);
+}
+
+template<typename T, int maxsize>
 bool Container<T, maxsize>::IsEmpty() const
+{
+	return (size == 0);
+}
+
+template<typename T, int maxsize>
+bool Container<T*, maxsize>::IsEmpty() const
 {
 	return (size == 0);
 }
@@ -96,6 +126,18 @@ int Container<T, maxsize>::find(T a) const
 }
 
 template<typename T, int maxsize>
+int Container<T*, maxsize>::find(T a) const
+{
+	if (IsEmpty()) throw "No elements in container";
+	for (int i = 0; i < size; i++)
+	{
+		if (**(arr + i) == a)
+			return i;
+	}
+	return -1;
+}
+
+template<typename T, int maxsize>
 void Container<T, maxsize>::Add(T a)
 {
 	if (IsFull()) throw "Container is full";
@@ -104,11 +146,28 @@ void Container<T, maxsize>::Add(T a)
 }
 
 template<typename T, int maxsize>
+void Container<T*, maxsize>::Add(T a)
+{
+	if (IsFull()) throw "Container is full";
+	arr[size] = new T(a);
+	size++;
+}
+
+template<typename T, int maxsize>
 void Container<T, maxsize>::Delete(T a)
 {
 	if (IsEmpty()) throw "No elements in container";
-	arr[find(a)] = arr[size - 1];
+	arr[find(a)] = arr[--size];
 	size--;
+}
+
+template<typename T, int maxsize>
+void Container<T*, maxsize>::Delete(T a)
+{
+	if (IsEmpty()) throw "No elements in container";
+	int tmp = find(a);
+	delete arr[tmp];
+	arr[tmp] = arr[--size];
 }
 
 template<typename T, int maxsize>
@@ -116,6 +175,13 @@ T& Container<T, maxsize>::operator[](int i)
 {
 	if (i > maxsize) throw "Out of range";
 	return arr[i];
+}
+
+template<typename T, int maxsize>
+T& Container<T*, maxsize>::operator[](int i)
+{
+	if (i > maxsize) throw "Out of range";
+	return *arr[i];
 }
 
 template<typename T, int maxsize>
@@ -128,22 +194,12 @@ void Container<T, maxsize>::print_all()
 	}
 }
 
-
-template <typename T, int maxsize>
-class Container<T*, maxsize>
+template<typename T, int maxsize>
+void Container<T*, maxsize>::print_all()
 {
-	T* arr;
-	int size;
-public:
-	Container();
-	Container(const Container<T*, maxsize>&);
-	~Container();
-	bool IsFull() const;
-	bool IsEmpty() const;
-	int find(T) const;
-	void Add(T);
-	void Delete(T);
-	T& operator[](int);
-	void print_all();
-};
-
+	if (IsEmpty()) throw "No elements in container";
+	for (int i = 0; i < size; i++)
+	{
+		cout << *arr[i] << endl;
+	}
+}
--- shagov_ma/Practice/Practice10/main.cpp
+++ shagov_ma/Practice/Practice10/main.cpp
@@ -2,20 +2,109 @@
 
 int main() 
 {
+	/*
 	Container<int, 10> a1;
+	cout << "Check Add" << endl;
+	for (int i = 0; i < 10; i++)
+	{
+		a1.Add(i);
+	}
+	a1.print_all();
+	cout << "Check a1[0]" << endl;
+	cout << a1[0] << endl;
+	cout << "Check delete a1[0]" << endl;
+	a1.Delete(0);
+	cout << "Check a1[0]" << endl;
+	cout << a1[0] << endl;
+	cout << "Check a1[0] = 100" << endl;
+	a1[0] = 100;
+	cout << a1[0] << endl;
+	Container<int, 10> a2(a1);
+	cout << "Check all a2" << endl;
+	a2.print_all();
 	try
 	{
-		//a1.print_all();
-		a1.Add(1);
-		cout << a1[0] << endl;
-		cin >> a1[0];
-		cout << a1[0] << endl;
+		cout << "Check a1[11] exception" << endl;
+		cout << a1[11] << endl;
 	}
-	catch (const char* k)
+	catch (const char *k)
 	{
-		std::cout << k << std::endl;
-		system("pause");
-		return 0;
+		cout << k << endl;
+	}
+	try
+	{
+		cout << "Check a1.Add(20) exception" << endl;
+		a1.Add(20);
+		a1.Add(20);
+	}
+	catch (const char *k)
+	{
+		cout << k << endl;
+	}
+	try
+	{
+		cout << "Check Delete exception" << endl;
+		for (int i = 0; i < 10; i++)
+		{
+			a1.Delete(i);
+		}
+		a1.Delete(0);
+	}
+	catch (const char *k)
+	{
+		cout << k << endl;
+	}
+	*/
+	Container<int*, 10> a1;
+	cout << "Check Add" << endl;
+	for (int i = 0; i < 10; i++)
+	{
+		a1.Add(i);
+	}
+	a1.print_all();
+	cout << "Check a1[0]" << endl;
+	cout << a1[0] << endl;
+	cout << "Check delete a1[0]" << endl;
+	a1.Delete(0);
+	cout << "Check a1[0]" << endl;
+	cout << a1[0] << endl;
+	cout << "Check a1[0] = 100" << endl;
+	a1[0] = 100;
+	cout << a1[0] << endl;
+	Container<int*, 10> a2(a1);
+	cout << "Check all a2" << endl;
+	a2.print_all();
+	try
+	{
+		cout << "Check a1[11] exception" << endl;
+		cout << a1[11] << endl;
+	}
+	catch (const char *k)
+	{
+		cout << k << endl;
+	}
+	try
+	{
+		cout << "Check a1.Add(20) exception" << endl;
+		a1.Add(20);
+		a1.Add(20);
+	}
+	catch (const char *k)
+	{
+		cout << k << endl;
+	}
+	try
+	{
+		cout << "Check Delete exception" << endl;
+		for (int i = 0; i < 10; i++)
+		{
+			a1.Delete(i);
+		}
+		a1.Delete(0);
+	}
+	catch (const char *k)
+	{
+		cout << k << endl;
 	}
 	system("pause");
 	return 0;
