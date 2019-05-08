2d353c28d3d06470dca4b02331cfb92e15a52907
MaksimShagov <43129418+MaksimShagov@users.noreply.github.com>
2019-05-08 19:27:56 +0300

    Update main.cpp

--- shagov_ma/Practice/Practice10/main.cpp
+++ shagov_ma/Practice/Practice10/main.cpp
@@ -2,7 +2,6 @@
 
 int main() 
 {
-	/*
 	Container<int, 10> a1;
 	cout << "Check Add" << endl;
 	for (int i = 0; i < 10; i++)
@@ -54,30 +53,29 @@ int main()
 	{
 		cout << k << endl;
 	}
-	*/
-	Container<int*, 10> a1;
+	Container<int*, 10> b1;
 	cout << "Check Add" << endl;
 	for (int i = 0; i < 10; i++)
 	{
-		a1.Add(i);
+		b1.Add(i);
 	}
-	a1.print_all();
-	cout << "Check a1[0]" << endl;
-	cout << a1[0] << endl;
-	cout << "Check delete a1[0]" << endl;
-	a1.Delete(0);
-	cout << "Check a1[0]" << endl;
-	cout << a1[0] << endl;
-	cout << "Check a1[0] = 100" << endl;
-	a1[0] = 100;
-	cout << a1[0] << endl;
-	Container<int*, 10> a2(a1);
-	cout << "Check all a2" << endl;
-	a2.print_all();
+	b1.print_all();
+	cout << "Check b1[0]" << endl;
+	cout << b1[0] << endl;
+	cout << "Check delete b1[0]" << endl;
+	b1.Delete(0);
+	cout << "Check b1[0]" << endl;
+	cout << b1[0] << endl;
+	cout << "Check b1[0] = 100" << endl;
+	b1[0] = 100;
+	cout << b1[0] << endl;
+	Container<int*, 10> b2(b1);
+	cout << "Check all b2" << endl;
+	b2.print_all();
 	try
 	{
-		cout << "Check a1[11] exception" << endl;
-		cout << a1[11] << endl;
+		cout << "Check b1[11] exception" << endl;
+		cout << b1[11] << endl;
 	}
 	catch (const char *k)
 	{
@@ -85,9 +83,9 @@ int main()
 	}
 	try
 	{
-		cout << "Check a1.Add(20) exception" << endl;
-		a1.Add(20);
-		a1.Add(20);
+		cout << "Check b1.Add(20) exception" << endl;
+		b1.Add(20);
+		b1.Add(20);
 	}
 	catch (const char *k)
 	{
@@ -98,9 +96,9 @@ int main()
 		cout << "Check Delete exception" << endl;
 		for (int i = 0; i < 10; i++)
 		{
-			a1.Delete(i);
+			b1.Delete(i);
 		}
-		a1.Delete(0);
+		b1.Delete(0);
 	}
 	catch (const char *k)
 	{
