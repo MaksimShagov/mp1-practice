b4e30ba30e868e5aea0d9e3ebdcd7889938393ca
MaksimShagov <43129418+MaksimShagov@users.noreply.github.com>
2019-06-02 14:02:04 +0300

    Practice6

--- shagov_ma/Practice/Practice6/Vector.h
+++ shagov_ma/Practice/Practice6/Vector.h
@@ -9,8 +9,7 @@ Vector create(int dim); // создание
 void delete(Vector a);// удаление
 Vector sum(Vector a, Vector b);//a+b
 Vector sub(Vector a, Vector b);
-double scalar(Vector a, Vector b);
-double angle(Vector a, Vector b);
-double length(Vector a);
+double scalar(Vector a, Vector b, int *flag);
+double angle(Vector a, Vector b, int *flag);
+double length(Vector a, int *flag);
 void print_vector(Vector a);
-Vector null();
