3fcde987ea62d48a56bdc30006d31fc9470fd715
MaksimShagov <43129418+MaksimShagov@users.noreply.github.com>
2019-06-03 14:47:11 +0300

    Add files via upload

--- shagov_ma/Practice/Practice6/Vector.h
+++ shagov_ma/Practice/Practice6/Vector.h
@@ -9,7 +9,7 @@ Vector create(int dim); // создание
 void delete(Vector a);// удаление
 Vector sum(Vector a, Vector b);//a+b
 Vector sub(Vector a, Vector b);
-double scalar(Vector a, Vector b, int *flag);
-double angle(Vector a, Vector b, int *flag);
-double length(Vector a, int *flag);
-void print_vector(Vector a);
+double scalar(Vector a, Vector b, double *scal) const;
+double angle(Vector a, Vector b, double *angl) const;
+double length(Vector a, double *leng) const;
+void print_vector(Vector a) const;
